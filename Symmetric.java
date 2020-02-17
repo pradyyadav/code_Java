d41f8652a9ed6aa0dd2d9128a2e23649984cc936
Prad250100 <pradyumna.aky@gmail.com>
2020-02-17 15:36:08 +0530

    User friendly

--- Symmetric.java
+++ Symmetric.java
@@ -1,28 +1,42 @@
 import java.util.Arrays;
+import java.util.Scanner;
 class Symmetric
 {
 	public static void main(String args[])
 	{
-		int count=0,a[][]={{1,4,5},{4,2,6},{5,6,3}};
-		System.out.println("Array is \n ");
-		for(int i=0;i<3;i++)
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter the order of matrix : ");
+		int n = scan.nextInt();
+		int a[][] = new int[n][n];
+		int count=0;
+		System.out.print("Enter the elements in the matrix\n\n");
+		for(int i = 0;i<n;i++)
 		{
-			for(int j=0;j<3;j++)
+			for(int j=0;j<n;j++)
 			{
-				System.out.println(a[i][j] + " ");
+				a[i][j]=scan.nextInt();
 			}
-			System.out.println("\n");
 		}
-		for(int i=0;i<3;i++)
+		System.out.println("\nArray is \n ");
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
 			{
-				for(int j=0;j<i;j++)
+				System.out.print(a[i][j] + " ");
+			}
+			System.out.print("\n");
+		}
+		for(int i=0;i<n;i++)
+			{
+				for(int j=0;j<=i;j++)
 				{
 					if(a[i][j]==a[j][i])
 					count++;
 					
 				}
 			}
-		if(count>=3)
+
+		if(count>n)
 		System.out.println("Symmetric");
 		else
 		System.out.println("not Symmetric");
