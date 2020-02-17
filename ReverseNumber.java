38c80f7496ec4d3f98e44122ba4c4857ac0c25a1
Prad250100 <pradyumna.aky@gmail.com>
2020-02-17 14:43:58 +0530

    Can take input

--- ReverseNumber.java
+++ ReverseNumber.java
@@ -1,6 +1,11 @@
-    public class ReverseNumber {
-        public static void main(String[] args) {
-            int num = 1234, reversed = 0;
+import java.util.Scanner;
+public class ReverseNumber {
+        public static void main(String[] args)
+        {
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Enter a number to get it reversed : ")
+            int num = scan.nextInt();
+            int reversed = 0;
             while(num != 0) {
                 int digit = num % 10;
                 reversed = reversed * 10 + digit;
