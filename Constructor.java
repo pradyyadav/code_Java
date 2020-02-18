d0d7626ea8a38bea911d5734c4336afefc07ba8d
Prad250100 <pradyumna.aky@gmail.com>
2020-02-18 23:11:52 +0530

    basic

---
+++ Constructor.java
@@ -0,0 +1,12 @@
+class Constructor
+{
+	Constructor()
+	{
+		System.out.println("Hi I am a Constructor !!!");
+		
+	}
+	public static void main(String args[])
+	{
+		Constructor ob = new Constructor();//Constructor block starts automatically when object is declared.
+	}
+}
